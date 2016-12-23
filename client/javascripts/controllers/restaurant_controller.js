767bd38275fe91542d6bdf79fd7729b219446abb
Anna Baldwin <anna.baldwin21@gmail.com>
2016-12-23 12:33:46 -0700

    seeded more meals

--- client/javascripts/controllers/restaurant_controller.js
+++ client/javascripts/controllers/restaurant_controller.js
@@ -9,7 +9,6 @@ app.controller('RestaurantController', function($scope, $routeParams, $location,
 
     mainService.getId.get({id: id}, function(returnedMeals) {
         $scope.view.mealsArray = returnedMeals
-        console.log('returnedMeals in restaurant dasbhoard:', returnedMeals);
     })
 
 
--- server/seeds/3_meals.js
+++ server/seeds/3_meals.js
@@ -12,14 +12,34 @@ exports.seed = function(knex) {
         price: 5.99
       },
       {
+        restaurant_id: 1,
+        user_id: null,
+        name: 'Lamb & Oat Meatloaf',
+        details: 'Triple m bar ranch lamb, grassfed beef, Elevation ketchup, pumpkin seed pesto, crispy sweet potato bites.',
+        dietary: 'gluten-free options',
+        pickup: 'Before close',
+        image: 'http://personaltraininginmarin.com/wp-content/uploads/2012/08/lamb-turkey-bison-meatloaf1.jpg',
+        price: 6.99
+      },
+      {
         restaurant_id: 2,
         user_id: null,
         name: 'Next Door Beet Burger',
         details: 'Local beet, and topped with arugula, balsamic onions, and feta.',
         dietary: 'Vegetarian and can request gluten-free bun.',
-        pickup: 'Aftter 7 pm',
+        pickup: 'Between 5 and 6 pm.',
         image: 'http://cf.shelikesfood.com/wp-content/uploads/2014/08/Amys-3422-683x1024.jpg',
         price: 3.99
+      },
+      {
+        restaurant_id: 2,
+        user_id: null,
+        name: 'Pineapple Express Burger',
+        details: 'Balsamic pineapple, swiss, bacon, arugula, and honey sriracha sauce.',
+        dietary: 'Gluten Free',
+        pickup: 'Between 5 and 6 pm.',
+        image: 'http://img.sndimg.com/food/image/upload/w_614,h_461,c_fit/v1/img/recipes/50/13/14/picT2mM6u.jpg',
+        price: 4.99
       },
       {
         restaurant_id: 3,
@@ -27,9 +47,19 @@ exports.seed = function(knex) {
         name: 'Buffalo Chicken Sandwich',
         details: 'Housemade pickles, blue cheese crumbles, and ranch dressing. Collard Greens side.',
         dietary: 'can request gluten-free bun.',
-        pickup: 'Aftter 7 pm',
+        pickup: 'After 7 pm',
         image: 'http://img.sndimg.com/food/image/upload/w_614,h_461,c_fit/v1/img/recipes/29/21/66/HC20RL1qSyCTtcXM8UBB_buffalo-chicken-sandwich-5755.jpg',
         price: 5.99
+      },
+      {
+        restaurant_id: 3,
+        user_id: null,
+        name: 'KC Half Rack of Ribs',
+        details: 'St. Louis cut, Kansas City BBQ sauce. Fries on the side.',
+        dietary: 'Dairy-free',
+        pickup: 'After 7 pm',
+        image: 'https://c1.staticflickr.com/7/6213/6332807985_1b05c82101_b.jpg',
+        price: 8.99
       }]);
     });
 };
